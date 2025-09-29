51f8584fa59820a732f6eb15f0d6432e0cc46a07
laurabrilhantee <laurabrilhante8@gmail.com>
2025-09-29 10:57:37 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [cidade, setCountCidade] = useState('');
   const [clima, setClima] = useState(null);
-  const [carregando, setCarregado] = useState(false);
+  const [carregando, setCarregando] = useState(false);
   const [erro, setErro] = useState('');
 
 //função para buscar dados db clima 
-const buscaclima = async () => {
+const buscaClima = async () => {
 
 if(!cidade.trim()){
   setErro('Por favor, digite uma cidade');
@@ -18,22 +16,41 @@ if(!cidade.trim()){
   }
 }
 
-  setCarregado(true);
+  setCarregando(true);
   setErro('');
 
 try{
+  const API_KEY = 'bd5e378503939ddaee76f12ad7a97608'
+  const url = `https://api.openweathermao.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&units=metric&lang=pt_br`;
+  const resposta = await fetch(url);
+
+  if (!resposta.ok){
+    throw new Error('Cidade não encontrada');
+  }
+
+  const dados = await resposta.json();
+  setClima(dados);
 
   } catch (error){
+    setErro(error.message);
+    setClima(null);
+  } finally {
+    setCarregando(false);
 
-  } finaly {
-    
-  }
-}
+  };
+    const handlekeyPress = (e) => {
+      if(e.key == 'Enter'){
+        buscaclima();
+      }
+    };
   return (
     <>
-   
+      <div className="app-container">
+      <div className="contant-wrapper">
+
+      </div>
+      </div>
     </>
   )
 }
-
 export default App
